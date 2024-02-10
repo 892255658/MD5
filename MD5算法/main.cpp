31834463d2c1cd2c42d0bcb6cd495485c19594d7
Ringo <892255658@qq.com>
2024-02-10 18:15:20 +0800

    2/10

---
+++ MD5算法/main.cpp
@@ -0,0 +1,12 @@
+﻿#include<iostream>
+using namespace std;
+//argc为命令行参数的个数
+int main(int argc,char* argv[])
+{
+	
+	system("pause");
+	return 0;
+}
+//MD5算法应用，注册时候将密码转换成MD5码上传给服务器，使入侵者无法得到源账号的密码
+//发送一个文件给服务器,服务器检测文件完整性
+//MD5中每个字节都参与了运算
